736b02ae0c675daa497aae1b7743ead22be47966
Tim Davis <DrTimothyALdenDavis@gmail.com>
2022-03-07 05:36:41 -0600

    remove old version of GraphBLAS

--- GraphBLAS/GraphBLAS/@GrB/private/gb_isfloat.m
+++
@@ -1,9 +0,0 @@
-function s = gb_isfloat (type)
-%GB_ISFLOAT true for floating-point GraphBLAS types.
-% Implements s = isfloat (type (G))
-
-% SuiteSparse:GraphBLAS, Timothy A. Davis, (c) 2017-2021, All Rights Reserved.
-% SPDX-License-Identifier: GPL-3.0-or-later
-
-s = contains (type, 'double') || contains (type, 'single') ;
-
