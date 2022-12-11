bb0879792b7cd87b58c5ccf6905c076f01676c5a
Tim Davis <davis@tamu.edu>
2022-12-11 16:21:31 -0600

    umfpack test coverage

---
+++ UMFPACK/Tcov/ut_dl.c
@@ -0,0 +1,11 @@
+//------------------------------------------------------------------------------
+// UMFPACK/Tcov/ut_dl.c:
+// double int64_t version of ut
+//------------------------------------------------------------------------------
+
+// UMFPACK, Copyright (c) 2005-2022, Timothy A. Davis, All Rights Reserved.
+// SPDX-License-Identifier: GPL-2.0+
+
+#define DLONG
+#include "ut.c"
+
---
+++ UMFPACK/Tcov/ut_zi.c
@@ -0,0 +1,11 @@
+//------------------------------------------------------------------------------
+// UMFPACK/Tcov/ut_zi.c:
+// complex int32_t version of ut
+//------------------------------------------------------------------------------
+
+// UMFPACK, Copyright (c) 2005-2022, Timothy A. Davis, All Rights Reserved.
+// SPDX-License-Identifier: GPL-2.0+
+
+#define ZINT
+#include "ut.c"
+
