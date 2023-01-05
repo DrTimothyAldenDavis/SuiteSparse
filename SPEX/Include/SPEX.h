952901b82304b23e3b5a9d82de6d2cb1b3e61a90
Tim Davis <davis@tamu.edu>
2023-01-05 12:09:51 -0600

    target date: Jan 9, 2023

--- SPEX/Include/SPEX.h
+++ SPEX/Include/SPEX.h
@@ -265,7 +265,7 @@
 // #define SPEX_UTIL_VERSION_MINOR 1
 // #define SPEX_UTIL_VERSION_SUB   1
 
-#define SPEX_DATE "Jan 1, 2023"
+#define SPEX_DATE "Jan 9, 2023"
 #define SPEX_VERSION "2.0.3"
 #define SPEX_VERSION_MAJOR 2
 #define SPEX_VERSION_MINOR 0
