15505493d3535ad7065d26959497c01226da6e82
Tim Davis <davis@tamu.edu>
2023-08-30 16:12:58 -0500

    GraphBLAS: user guide and PreJIT kernels

--- GraphBLAS/Tcov/PreJIT/GB_jit__AxB_dot2__2c1f100bba0bbac7__plus_my_rdiv2.c
+++ GraphBLAS/Tcov/PreJIT/GB_jit__AxB_dot2__2c1f100bba0bbac7__plus_my_rdiv2.c
@@ -123,7 +123,7 @@ GB_JIT_GLOBAL GB_JIT_QUERY_PROTO (GB_jit_query) ;
 GB_JIT_GLOBAL GB_JIT_QUERY_PROTO (GB_jit_query)
 {
     (*hash) = 0xfaa3c6cd7f90ec16 ;
-    v [0] = 8 ; v [1] = 0 ; v [2] = 3 ;
+    v [0] = 8 ; v [1] = 2 ; v [2] = 0 ;
     defn [0] = NULL ;
     defn [1] = GB_my_rdiv2_USER_DEFN ;
     defn [2] = NULL ;
