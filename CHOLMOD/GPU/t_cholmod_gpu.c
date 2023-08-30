d0878aeedca940675c8bfe950311f3c5e9441311
whuaegeansea <whuaegeansea@gmail.com>
2023-08-30 23:15:23 +0800

    Fix building on windows

--- CHOLMOD/GPU/t_cholmod_gpu.c
+++ CHOLMOD/GPU/t_cholmod_gpu.c
@@ -117,13 +117,13 @@ int TEMPLATE2 (CHOLMOD (gpu_init))
 
     /* divvy up the memory in dev_mempool */
     gpu_p->d_Lx[0] = Common->dev_mempool;
-    gpu_p->d_Lx[1] = Common->dev_mempool + Common->devBuffSize;
-    gpu_p->d_C = Common->dev_mempool + 2*Common->devBuffSize;
-    gpu_p->d_A[0] = Common->dev_mempool + 3*Common->devBuffSize;
-    gpu_p->d_A[1] = Common->dev_mempool + 4*Common->devBuffSize;
-    gpu_p->d_Ls = Common->dev_mempool + 5*Common->devBuffSize;
-    gpu_p->d_Map = gpu_p->d_Ls + (nls+1)*sizeof(int64_t) ;
-    gpu_p->d_RelativeMap = gpu_p->d_Map + (n+1)*sizeof(int64_t) ;
+    gpu_p->d_Lx[1] = (char*)Common->dev_mempool + Common->devBuffSize;
+    gpu_p->d_C = (char*)Common->dev_mempool + 2 * Common->devBuffSize;
+    gpu_p->d_A[0] = (char*)Common->dev_mempool + 3 * Common->devBuffSize;
+    gpu_p->d_A[1] = (char*)Common->dev_mempool + 4 * Common->devBuffSize;
+    gpu_p->d_Ls = (char*)Common->dev_mempool + 5 * Common->devBuffSize;
+    gpu_p->d_Map = (char*)gpu_p->d_Ls + (nls + 1) * sizeof(int64_t);
+    gpu_p->d_RelativeMap = (char*)gpu_p->d_Map + (n + 1) * sizeof(int64_t) ;
 
     /* Copy all of the Ls and Lpi data to the device.  If any supernodes are
      * to be computed on the device then this will be needed, so might as
