DrTimothyAldenDavis/SuiteSparse

commit 42d82d857d60551fca4108cddd8e5ba018c9a1f8

Support installing package configuration files at different location
This change allows the user to define a location for the CMake and
pkg-config configuration files while installing the libraries, e.g., at
a "default" location.

This will hopefully help for the use case described in #425.

GraphBLAS/cmake_modules/SuiteSparsePolicy.cmake
@@ -174,11 +174,16 @@ if ( INSIDE_SUITESPARSE )
     set ( CMAKE_BUILD_RPATH   ${CMAKE_BUILD_RPATH}   ${SUITESPARSE_LIBDIR} )
 endif ( )
 
-message ( STATUS "Install lib:     ${SUITESPARSE_LIBDIR}" )
-message ( STATUS "Install include: ${SUITESPARSE_INCLUDEDIR}" )
-message ( STATUS "Install bin:     ${SUITESPARSE_BINDIR}" )
-message ( STATUS "Install rpath:   ${CMAKE_INSTALL_RPATH}" )
-message ( STATUS "Build   rpath:   ${CMAKE_BUILD_RPATH}" )
+option ( SUITESPARSE_PKGFILEDIR
+    "Directory where CMake Config and pkg-config files will be installed"
+    ${SUITESPARSE_LIBDIR} )
+
+message ( STATUS "Install lib:      ${SUITESPARSE_LIBDIR}" )
+message ( STATUS "Install include:  ${SUITESPARSE_INCLUDEDIR}" )
+message ( STATUS "Install bin:      ${SUITESPARSE_BINDIR}" )
+message ( STATUS "Install pkg-file: ${SUITESPARSE_PKGFILEDIR}" )
+message ( STATUS "Install rpath:    ${CMAKE_INSTALL_RPATH}" )
+message ( STATUS "Build   rpath:    ${CMAKE_BUILD_RPATH}" )
 
 if ( NOT CMAKE_BUILD_TYPE )
     set ( CMAKE_BUILD_TYPE Release )

SuiteSparse_config/cmake_modules/SuiteSparsePolicy.cmake
@@ -174,11 +174,15 @@ if ( INSIDE_SUITESPARSE )
     set ( CMAKE_BUILD_RPATH   ${CMAKE_BUILD_RPATH}   ${SUITESPARSE_LIBDIR} )
 endif ( )
 
-message ( STATUS "Install lib:     ${SUITESPARSE_LIBDIR}" )
-message ( STATUS "Install include: ${SUITESPARSE_INCLUDEDIR}" )
-message ( STATUS "Install bin:     ${SUITESPARSE_BINDIR}" )
-message ( STATUS "Install rpath:   ${CMAKE_INSTALL_RPATH}" )
-message ( STATUS "Build   rpath:   ${CMAKE_BUILD_RPATH}" )
+set ( SUITESPARSE_PKGFILEDIR ${SUITESPARSE_LIBDIR} CACHE STRING
+    "Directory where CMake Config and pkg-config files will be installed" )
+
+message ( STATUS "Install lib:      ${SUITESPARSE_LIBDIR}" )
+message ( STATUS "Install include:  ${SUITESPARSE_INCLUDEDIR}" )
+message ( STATUS "Install bin:      ${SUITESPARSE_BINDIR}" )
+message ( STATUS "Install pkg-file: ${SUITESPARSE_PKGFILEDIR}" )
+message ( STATUS "Install rpath:    ${CMAKE_INSTALL_RPATH}" )
+message ( STATUS "Build   rpath:    ${CMAKE_BUILD_RPATH}" )
 
 if ( NOT CMAKE_BUILD_TYPE )
     set ( CMAKE_BUILD_TYPE Release )

AMD/CMakeLists.txt
@@ -171,7 +171,7 @@ export ( EXPORT AMDTargets
 # install export target, config and version files for find_package
 install ( EXPORT AMDTargets
     NAMESPACE SuiteSparse::
-    DESTINATION ${SUITESPARSE_LIBDIR}/cmake/AMD )
+    DESTINATION ${SUITESPARSE_PKGFILEDIR}/cmake/AMD )
 
 # generate config file to be used in common build tree
 set ( SUITESPARSE_IN_BUILD_TREE ON )
@@ -185,7 +185,7 @@ set ( SUITESPARSE_IN_BUILD_TREE OFF )
 configure_package_config_file (
     Config/AMDConfig.cmake.in
     ${CMAKE_CURRENT_BINARY_DIR}/target/AMDConfig.cmake
-    INSTALL_DESTINATION ${SUITESPARSE_LIBDIR}/cmake/AMD )
+    INSTALL_DESTINATION ${SUITESPARSE_PKGFILEDIR}/cmake/AMD )
 
 write_basic_package_version_file (
     ${CMAKE_CURRENT_BINARY_DIR}/AMDConfigVersion.cmake
@@ -194,7 +194,7 @@ write_basic_package_version_file (
 install ( FILES
     ${CMAKE_CURRENT_BINARY_DIR}/AMDConfig.cmake
     ${CMAKE_CURRENT_BINARY_DIR}/AMDConfigVersion.cmake
-    DESTINATION ${SUITESPARSE_LIBDIR}/cmake/AMD )
+    DESTINATION ${SUITESPARSE_PKGFILEDIR}/cmake/AMD )
 
 #-------------------------------------------------------------------------------
 # create pkg-config file
@@ -222,7 +222,7 @@ if ( NOT MSVC )
         NEWLINE_STYLE LF )
     install ( FILES
         ${CMAKE_CURRENT_BINARY_DIR}/AMD.pc
-        DESTINATION ${SUITESPARSE_LIBDIR}/pkgconfig )
+        DESTINATION ${SUITESPARSE_PKGFILEDIR}/pkgconfig )
 endif ( )
 
 #-------------------------------------------------------------------------------

BTF/CMakeLists.txt
@@ -159,7 +159,7 @@ export ( EXPORT BTFTargets
 # install export target, config and version files for find_package
 install ( EXPORT BTFTargets
     NAMESPACE SuiteSparse::
-    DESTINATION ${SUITESPARSE_LIBDIR}/cmake/BTF )
+    DESTINATION ${SUITESPARSE_PKGFILEDIR}/cmake/BTF )
 
 # generate config file to be used in common build tree
 set ( SUITESPARSE_IN_BUILD_TREE ON )
@@ -173,7 +173,7 @@ set ( SUITESPARSE_IN_BUILD_TREE OFF )
 configure_package_config_file (
     Config/BTFConfig.cmake.in
     ${CMAKE_CURRENT_BINARY_DIR}/target/BTFConfig.cmake
-    INSTALL_DESTINATION ${SUITESPARSE_LIBDIR}/cmake/BTF )
+    INSTALL_DESTINATION ${SUITESPARSE_PKGFILEDIR}/cmake/BTF )
 
 write_basic_package_version_file (
     ${CMAKE_CURRENT_BINARY_DIR}/BTFConfigVersion.cmake
@@ -182,7 +182,7 @@ write_basic_package_version_file (
 install ( FILES
     ${CMAKE_CURRENT_BINARY_DIR}/BTFConfig.cmake
     ${CMAKE_CURRENT_BINARY_DIR}/BTFConfigVersion.cmake
-    DESTINATION ${SUITESPARSE_LIBDIR}/cmake/BTF )
+    DESTINATION ${SUITESPARSE_PKGFILEDIR}/cmake/BTF )
 
 #-------------------------------------------------------------------------------
 # create pkg-config file
@@ -210,7 +210,7 @@ if ( NOT MSVC )
         NEWLINE_STYLE LF )
     install ( FILES
         ${CMAKE_CURRENT_BINARY_DIR}/BTF.pc
-        DESTINATION ${SUITESPARSE_LIBDIR}/pkgconfig )
+        DESTINATION ${SUITESPARSE_PKGFILEDIR}/pkgconfig )
 endif ( )
 
 #-------------------------------------------------------------------------------

CAMD/CMakeLists.txt
@@ -161,7 +161,7 @@ export ( EXPORT CAMDTargets
 # install export target, config and version files for find_package
 install ( EXPORT CAMDTargets
     NAMESPACE SuiteSparse::
-    DESTINATION ${SUITESPARSE_LIBDIR}/cmake/CAMD )
+    DESTINATION ${SUITESPARSE_PKGFILEDIR}/cmake/CAMD )
 
 # generate config file to be used in common build tree
 set ( SUITESPARSE_IN_BUILD_TREE ON )
@@ -175,7 +175,7 @@ set ( SUITESPARSE_IN_BUILD_TREE OFF )
 configure_package_config_file (
     Config/CAMDConfig.cmake.in
     ${CMAKE_CURRENT_BINARY_DIR}/target/CAMDConfig.cmake
-    INSTALL_DESTINATION ${SUITESPARSE_LIBDIR}/cmake/CAMD )
+    INSTALL_DESTINATION ${SUITESPARSE_PKGFILEDIR}/cmake/CAMD )
 
 write_basic_package_version_file (
     ${CMAKE_CURRENT_BINARY_DIR}/CAMDConfigVersion.cmake
@@ -184,7 +184,7 @@ write_basic_package_version_file (
 install ( FILES
     ${CMAKE_CURRENT_BINARY_DIR}/CAMDConfig.cmake
     ${CMAKE_CURRENT_BINARY_DIR}/CAMDConfigVersion.cmake
-    DESTINATION ${SUITESPARSE_LIBDIR}/cmake/CAMD )
+    DESTINATION ${SUITESPARSE_PKGFILEDIR}/cmake/CAMD )
 
 #-------------------------------------------------------------------------------
 # create pkg-config file
@@ -212,7 +212,7 @@ if ( NOT MSVC )
         NEWLINE_STYLE LF )
     install ( FILES
         ${CMAKE_CURRENT_BINARY_DIR}/CAMD.pc
-        DESTINATION ${SUITESPARSE_LIBDIR}/pkgconfig )
+        DESTINATION ${SUITESPARSE_PKGFILEDIR}/pkgconfig )
 endif ( )
 
 #-------------------------------------------------------------------------------

CCOLAMD/CMakeLists.txt
@@ -158,7 +158,7 @@ export ( EXPORT CCOLAMDTargets
 # install export target, config and version files for find_package
 install ( EXPORT CCOLAMDTargets
     NAMESPACE SuiteSparse::
-    DESTINATION ${SUITESPARSE_LIBDIR}/cmake/CCOLAMD )
+    DESTINATION ${SUITESPARSE_PKGFILEDIR}/cmake/CCOLAMD )
 
 # generate config file to be used in common build tree
 set ( SUITESPARSE_IN_BUILD_TREE ON )
@@ -172,7 +172,7 @@ set ( SUITESPARSE_IN_BUILD_TREE OFF )
 configure_package_config_file (
     Config/CCOLAMDConfig.cmake.in
     ${CMAKE_CURRENT_BINARY_DIR}/target/CCOLAMDConfig.cmake
-    INSTALL_DESTINATION ${SUITESPARSE_LIBDIR}/cmake/CCOLAMD )
+    INSTALL_DESTINATION ${SUITESPARSE_PKGFILEDIR}/cmake/CCOLAMD )
 
 write_basic_package_version_file (
     ${CMAKE_CURRENT_BINARY_DIR}/CCOLAMDConfigVersion.cmake
@@ -181,7 +181,7 @@ write_basic_package_version_file (
 install ( FILES
     ${CMAKE_CURRENT_BINARY_DIR}/CCOLAMDConfig.cmake
     ${CMAKE_CURRENT_BINARY_DIR}/CCOLAMDConfigVersion.cmake
-    DESTINATION ${SUITESPARSE_LIBDIR}/cmake/CCOLAMD )
+    DESTINATION ${SUITESPARSE_PKGFILEDIR}/cmake/CCOLAMD )
 
 #-------------------------------------------------------------------------------
 # create pkg-config file
@@ -209,7 +209,7 @@ if ( NOT MSVC )
         NEWLINE_STYLE LF )
     install ( FILES
         ${CMAKE_CURRENT_BINARY_DIR}/CCOLAMD.pc
-        DESTINATION ${SUITESPARSE_LIBDIR}/pkgconfig )
+        DESTINATION ${SUITESPARSE_PKGFILEDIR}/pkgconfig )
 endif ( )
 
 #-------------------------------------------------------------------------------

CHOLMOD/CMakeLists.txt
@@ -531,7 +531,7 @@ export ( EXPORT CHOLMODTargets
 # install export target, config and version files for find_package
 install ( EXPORT CHOLMODTargets
     NAMESPACE SuiteSparse::
-    DESTINATION ${SUITESPARSE_LIBDIR}/cmake/CHOLMOD )
+    DESTINATION ${SUITESPARSE_PKGFILEDIR}/cmake/CHOLMOD )
 
 # generate config file to be used in common build tree
 set ( SUITESPARSE_IN_BUILD_TREE on )
@@ -545,7 +545,7 @@ set ( SUITESPARSE_IN_BUILD_TREE off )
 configure_package_config_file (
     Config/CHOLMODConfig.cmake.in
     ${CMAKE_CURRENT_BINARY_DIR}/target/CHOLMODConfig.cmake
-    INSTALL_DESTINATION ${SUITESPARSE_LIBDIR}/cmake/CHOLMOD )
+    INSTALL_DESTINATION ${SUITESPARSE_PKGFILEDIR}/cmake/CHOLMOD )
 
 write_basic_package_version_file (
     ${CMAKE_CURRENT_BINARY_DIR}/CHOLMODConfigVersion.cmake
@@ -554,7 +554,7 @@ write_basic_package_version_file (
 install ( FILES
     ${CMAKE_CURRENT_BINARY_DIR}/target/CHOLMODConfig.cmake
     ${CMAKE_CURRENT_BINARY_DIR}/CHOLMODConfigVersion.cmake
-    DESTINATION ${SUITESPARSE_LIBDIR}/cmake/CHOLMOD )
+    DESTINATION ${SUITESPARSE_PKGFILEDIR}/cmake/CHOLMOD )
 
 #-------------------------------------------------------------------------------
 # create pkg-config file
@@ -609,7 +609,7 @@ if ( NOT MSVC )
         NEWLINE_STYLE LF )
     install ( FILES
         ${CMAKE_CURRENT_BINARY_DIR}/CHOLMOD.pc
-        DESTINATION ${SUITESPARSE_LIBDIR}/pkgconfig )
+        DESTINATION ${SUITESPARSE_PKGFILEDIR}/pkgconfig )
 endif ( )
 
 #-------------------------------------------------------------------------------

CHOLMOD/GPU/CMakeLists.txt
@@ -120,12 +120,12 @@ export ( EXPORT CHOLMOD_CUDATargets
 # install export target, config and version files for find_package
 install ( EXPORT CHOLMOD_CUDATargets
     NAMESPACE SuiteSparse::
-    DESTINATION ${SUITESPARSE_LIBDIR}/cmake/CHOLMOD_CUDA )
+    DESTINATION ${SUITESPARSE_PKGFILEDIR}/cmake/CHOLMOD_CUDA )
 
 configure_package_config_file (
     ../Config/CHOLMOD_CUDAConfig.cmake.in
     ${CMAKE_CURRENT_BINARY_DIR}/../CHOLMOD_CUDAConfig.cmake
-    INSTALL_DESTINATION ${SUITESPARSE_LIBDIR}/cmake/CHOLMOD_CUDA )
+    INSTALL_DESTINATION ${SUITESPARSE_PKGFILEDIR}/cmake/CHOLMOD_CUDA )
 
 write_basic_package_version_file (
     ${CMAKE_CURRENT_BINARY_DIR}/../CHOLMOD_CUDAConfigVersion.cmake
@@ -134,7 +134,7 @@ write_basic_package_version_file (
 install ( FILES
     ${CMAKE_CURRENT_BINARY_DIR}/../CHOLMOD_CUDAConfig.cmake
     ${CMAKE_CURRENT_BINARY_DIR}/../CHOLMOD_CUDAConfigVersion.cmake
-    DESTINATION ${SUITESPARSE_LIBDIR}/cmake/CHOLMOD_CUDA )
+    DESTINATION ${SUITESPARSE_PKGFILEDIR}/cmake/CHOLMOD_CUDA )
 
 #-------------------------------------------------------------------------------
 # create pkg-config file
@@ -162,5 +162,5 @@ if ( NOT MSVC )
         NEWLINE_STYLE LF )
     install ( FILES
         ${CMAKE_CURRENT_BINARY_DIR}/CHOLMOD_CUDA.pc
-        DESTINATION ${SUITESPARSE_LIBDIR}/pkgconfig )
+        DESTINATION ${SUITESPARSE_PKGFILEDIR}/pkgconfig )
 endif ( )

commit 5bea47a3e034ac3d61c0f84be07ba7d3d7186031

GraphBLAS: Fix error in SuiteSparsePolicy.cmake
Commit 42d82d857d60551fca4108cddd8e5ba018c9a1f8 accidentally contained
a wrong version of that file. Synchronize it with the version in
SuiteSparse_config.

GraphBLAS/cmake_modules/SuiteSparsePolicy.cmake
@@ -174,9 +174,8 @@ if ( INSIDE_SUITESPARSE )
     set ( CMAKE_BUILD_RPATH   ${CMAKE_BUILD_RPATH}   ${SUITESPARSE_LIBDIR} )
 endif ( )
 
-option ( SUITESPARSE_PKGFILEDIR
-    "Directory where CMake Config and pkg-config files will be installed"
-    ${SUITESPARSE_LIBDIR} )
+set ( SUITESPARSE_PKGFILEDIR ${SUITESPARSE_LIBDIR} CACHE STRING
+    "Directory where CMake Config and pkg-config files will be installed" )
 
 message ( STATUS "Install lib:      ${SUITESPARSE_LIBDIR}" )
 message ( STATUS "Install include:  ${SUITESPARSE_INCLUDEDIR}" )