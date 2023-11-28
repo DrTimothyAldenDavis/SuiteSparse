a33abc745a31438288b30cce103fb66571290a23
Tim Davis <davis@tamu.edu>
2023-11-27 22:16:48 -0600

    LAGraph 1.1.0: version update

--- LAGraph/include/LAGraph.h
+++ LAGraph/include/LAGraph.h
@@ -37,10 +37,10 @@
 // See also the LAGraph_Version utility method, which returns these values.
 // These definitions are derived from LAGraph/CMakeLists.txt.
 
-#define LAGRAPH_DATE "Aug 2, 2023"
+#define LAGRAPH_DATE "FIXME:DATE, 2023"
 #define LAGRAPH_VERSION_MAJOR  1
-#define LAGRAPH_VERSION_MINOR  0
-#define LAGRAPH_VERSION_UPDATE 2
+#define LAGRAPH_VERSION_MINOR  1
+#define LAGRAPH_VERSION_UPDATE 0
 
 //==============================================================================
 // include files and helper macros
