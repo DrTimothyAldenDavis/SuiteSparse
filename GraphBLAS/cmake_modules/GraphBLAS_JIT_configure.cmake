e38845a394ba3d6c39aed6009a40bfe85e05ddcf
Tim Davis <DrTimothyAldenDavis@gmail.com>
2023-12-19 19:46:23 -0600

    NJIT and COMPACT renamed to GRAPHBLAS_*

--- GraphBLAS/cmake_modules/GraphBLAS_JIT_configure.cmake
+++ GraphBLAS/cmake_modules/GraphBLAS_JIT_configure.cmake
@@ -108,7 +108,7 @@ else ( )
 
 endif ( )
 
-if ( NOT NJIT OR GRAPHBLAS_USE_CUDA )
+if ( GRAPHBLAS_USE_JIT OR GRAPHBLAS_USE_CUDA )
     message ( STATUS "------------------------------------------------------------------------" )
     message ( STATUS "JIT configuration:" )
     message ( STATUS "------------------------------------------------------------------------" )
