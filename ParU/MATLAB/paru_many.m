d6f8b05a6488d5306824f09637be9641cd0d5325
Tim Davis <davis@tamu.edu>
2024-05-01 22:05:15 -0500

    testing

--- ParU/MATLAB/paru_many.m
+++ ParU/MATLAB/paru_many.m
@@ -435,6 +435,11 @@ singular_matrices = [
          979
         2303
         2590
+        2378
+        2318
+        2502
+        2510
+        2379
     ] ;
 
 % these matrices cause METIS to fail
