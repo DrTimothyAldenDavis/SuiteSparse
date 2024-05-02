c0ffdfc894dde5db482c3dcaf289f9ec9a2fcd7e
Tim Davis <davis@tamu.edu>
2024-05-02 08:30:19 -0500

    rename MATLAB folder to ParU

---
+++ ParU/ParU/Contents.m
@@ -0,0 +1,12 @@
+% ParU MATLAB interface
+%
+% ParU, Copyright (c) 2022-2024, Mohsen Aznaveh and Timothy A. Davis,
+% All Rights Reserved.
+% SPDX-License-Identifier: GPL-3.0-or-later
+%
+% Files
+%   paru      - solve Ax=b using ParU sparse LU factorization
+%   paru_demo - test a single sparse matrix in ParU
+%   paru_make - compiles the ParU mexFunction
+%   paru_many - test many matrices with ParU
+%   paru_tiny - test a tiny sparse matrix with ParU
