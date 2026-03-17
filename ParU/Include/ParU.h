4576e74a4874275c06d1dbf1e5318791329e0972
Tim Davis <DrTimothyAldenDavis@gmail.com>
2026-03-17 14:33:46 -0500

    ParU MATLAB mexFunction: do not rely on mkl.h

--- ParU/Include/ParU.h
+++ ParU/Include/ParU.h
@@ -53,25 +53,25 @@ typedef enum ParU_Info
     PARU_TOO_LARGE = -4         // problem too large for the BLAS
 } ParU_Info ;
 
-#define PARU_DATE "Nov 1, 2025"
+#define PARU_DATE "FIXME, 2026"
 #define PARU_VERSION_MAJOR  1
 #define PARU_VERSION_MINOR  1
-#define PARU_VERSION_UPDATE 0
+#define PARU_VERSION_UPDATE 1
 
-#define PARU__VERSION SUITESPARSE__VERCODE(1,1,0)
+#define PARU__VERSION SUITESPARSE__VERCODE(1,1,1)
 #if !defined (SUITESPARSE__VERSION) || \
     (SUITESPARSE__VERSION < SUITESPARSE__VERCODE(7,12,0))
-#error "ParU 1.1.0 requires SuiteSparse_config 7.12.0 or later"
+#error "ParU 1.1.1 requires SuiteSparse_config 7.12.0 or later"
 #endif
 
 #if !defined (UMFPACK__VERSION) || \
     (UMFPACK__VERSION < SUITESPARSE__VERCODE(6,3,7))
-#error "ParU 1.1.0 requires UMFPACK 6.3.7 or later"
+#error "ParU 1.1.1 requires UMFPACK 6.3.7 or later"
 #endif
 
 #if !defined (CHOLMOD__VERSION) || \
     (CHOLMOD__VERSION < SUITESPARSE__VERCODE(5,3,4))
-#error "ParU 1.1.0 requires CHOLMOD 5.3.4 or later"
+#error "ParU 1.1.1 requires CHOLMOD 5.3.4 or later"
 #endif
 
 //  the same values as UMFPACK_STRATEGY defined in UMFPACK/Include/umfpack.h
