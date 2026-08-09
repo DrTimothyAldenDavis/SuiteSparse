ef721dcead6bcface71d8932a91304b1e70a16ea
Tim Davis <drtimothyaldendavis@gmail.com>
2026-08-09 15:36:24 -0500

    SuiteSparse 7.13.0: date and last polish before release

--- Example/Include/my_internal.h
+++ Example/Include/my_internal.h
@@ -56,8 +56,8 @@
 #if ! defined (NO_GRAPHBLAS)
     #include "GraphBLAS.h"
     #if !defined ( GxB_SUITESPARSE_GRAPHBLAS ) || \
-        GxB_IMPLEMENTATION < GxB_VERSION (10,4,0)
-    #error "This library requires SuiteSparse:GraphBLAS 10.4.0 or later"
+        GxB_IMPLEMENTATION < GxB_VERSION (10,4,1)
+    #error "This library requires SuiteSparse:GraphBLAS 10.4.1 or later"
     #endif
 #endif
 
